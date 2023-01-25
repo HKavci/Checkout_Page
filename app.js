1ad6f131438406926fde6d191f41fc404fa00e5d
HakanKavci <hackankavci0@gmail.com>
2023-01-25 16:53:14 +0300

    total price ve cart total eklendi.

--- app.js
+++ app.js
@@ -16,29 +16,51 @@ window.addEventListener("load", ()=> {
 
 const container = document.querySelector(".container")
 
-let count = document.querySelector(".counter").innerText
 
 container.addEventListener("click", (e)=>{
     e.preventDefault()
     if (e.target.classList.contains("plus")) {
-        e.target.previousElementSibling.innerText++
-        e.target.closest(".foto-price").querySelector(".price").innerText = (e.target.closest(".foto-price").querySelector("h3 span").innerText * e.target.previousElementSibling.innerText).toFixed(2)     
+        e.target.previousElementSibling.innerText++    
+        calculateProductTotal(e)
     } else if (e.target.classList.contains("minus")) {
         if (e.target.nextElementSibling.innerText>1) {
-            e.target.nextElementSibling.innerText--
-            e.target.closest(".foto-price").querySelector(".price").innerText = (e.target.closest(".foto-price").querySelector("h3 span").innerText * e.target.nextElementSibling.innerText).toFixed(2)  
+            e.target.nextElementSibling.innerText--  
+            calculateProductTotal(e)
         }
     } else if (e.target.classList.contains("remove")) {
         e.target.parentElement.parentElement.parentElement.remove()
+        
     }
+    calculateCartPrice()
 })
 
 
+const calculateProductTotal = (e) => {
+    e.target.closest(".foto-price").querySelector(".price").innerText = 
+    (e.target.closest(".foto-price").querySelector(".counter").innerText * e.target.closest(".foto-price").querySelector("h3 span").innerText).toFixed(2)
+}
 
 
+const subTotal = document.getElementById("subtotal")
+const tax = document.getElementById("tax")
+const total = document.getElementById("sum")
+const shipping = document.getElementById("shipping")
 
 const calculateCartPrice = () => {
+    const productTotals = document.querySelectorAll(".price")
+    let totalPrice = 0
+    productTotals.forEach (price=>{
+        totalPrice += parseFloat(price.innerText)
+    })
+    subTotal.innerText = totalPrice.toFixed(2)
 
+    tax.innerText = (subTotal.innerText * 0.18).toFixed(2)
+
+    if (productTotals.length === 0) {
+        shipping.innerText = 0
+    }
+
+    total.innerText = parseFloat(subTotal.innerText + tax.innerText + shipping.innerText).toFixed(2)
 }
 
 
